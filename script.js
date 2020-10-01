4682ca246856d25b58d469ad9a592d3a4ee6e740
Aman Aujla <aujla7736@gmail.com>
2020-10-01 15:55:09 -0500

    updated code

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() { 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,6 +9,51 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  // password array
+
+  var password = [];
+
+  //character options
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericalcharacters = "0123456789";
+  var specialcharacters ="!@#$%^&*";
+
+// tests
+
+  console.log(lowercase)
+  console.log(uppercase)
+  console.log(specialcharacters)
+
+  // var plength = 8;
+
+  var plength = prompt("Please enter how long you want your password", "8");
+
+
+//promt the users to use lowercase
+//prompt the users to use uppercase
+//prompt the users to use numberical
+//prompt the users to use specialcharacters 
+//construct pool of characters
+
+// for loop - picking random characters for the password 
+
+
+
+for (var i = 0; i < plength; i++) {
+
+  //make random index
+  var randomindex = Math.floor(Math.random()*lowercase.length);
+//test
+  console.log(randomindex);
+
+  //add random characters to the password array
+  password.push(lowercase[randomindex],uppercase[randomindex],numericalcharacters[randomindex],specialcharacters[randomindex])
+}
+console.log(password);
+return password.join("");
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
